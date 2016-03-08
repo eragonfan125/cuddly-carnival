93035fff0b7b2e869626a67eae61733bd9e97624
eragonfan125 <hiworld125@gmail.com>
2016-03-08 15:45:00 -0800

    Update render.c

--- render.c
+++ render.c
@@ -38,7 +38,9 @@ void render2d(alls *all) {
   double r_A = all->p.p_A - all->s.p_FOV / 2;
   int i;
   for (i = 0; i < all->rn.cols; ++i) {
-    printcol(all, i, (int)(p_raylen(all, r_A) == -1 ? 0 : (all->rn.rows / p_raylen(all, r_A) == 0 ? all->rn.rows : all->rn.rows / p_raylen(all, r_A))));
+    printcol(all, i, (int)(\
+    p_raylen(all, r_A) == -1 ? 0 : \
+    p_raylen(all, r_A) < 1 ? all->rn.rows : all->rn.rows / p_raylen(all, r_A)));
     r_A += all->s.p_FOV / all->rn.cols;
   }
 }
